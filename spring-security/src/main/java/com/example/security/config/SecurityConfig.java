dd776276078cdc4a1bfc8ba0b2f84aa33637a2b0
rajkumarpr <raju.raigonde@gmail.com>
2025-02-26 22:21:09 +0530

    Customised application instead of using default spring security import

--- spring-security/src/main/java/com/example/security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class SecurityConfig {
